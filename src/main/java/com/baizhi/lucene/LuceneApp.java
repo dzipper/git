95f3c171e21caa62f0397a1c3d211869e8d3610b
sk <songk1208@126.com>
2018-08-09 19:05:46 +0800

    springboot,lucene环境，入口类

---
+++ src/main/java/com/baizhi/lucene/LuceneApp.java
@@ -0,0 +1,14 @@
+package com.baizhi.lucene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by SK on 2018/8/9.
+ */
+@SpringBootApplication
+public class LuceneApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LuceneApp.class,args);
+    }
+}
---
+++ src/main/java/com/baizhi/lucene/entity/Product.java
@@ -0,0 +1,7 @@
+package com.baizhi.lucene.entity;
+
+/**
+ * Created by SK on 2018/8/9.
+ */
+public class Product {
+}
